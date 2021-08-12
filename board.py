06d520ed3987c1ddce8611c65640e89d2e8f8d82
samtaaghol <35299367+samtaaghol@users.noreply.github.com>
2021-08-12 08:30:58 +0100

    Added functionality

--- board.py
+++ board.py
@@ -1,43 +1,59 @@
 from pieces import Pawn, Rook, Knight, Bishop, Queen, King
-from itertools import zip_longest
 import numpy as np
 
+# This function couples one element with a list.
+# e.g coupler(1, [1,2,3,4,5]) -> [(1,1), (1,2), (1,3), (1,4), (1,5)]
+coupler = lambda pos, moves : map(lambda move : (pos, move), moves)
+
 class Board:
-    
     def __init__(self):
         
         self.current_color = -1
         self.board = []
-        self.squares_attacked_by_white = []
-        self.squares_attacked_by_black = []
+        self.white_moves = []
+        self.black_moves = []
+        self.setup_board()
         self.setup_pieces()
+        
+        # Stores the piece objects
+        self.pieces = [[p for p in row if p != None] for row in self.board]
     
-    def setup_pieces(self):
+    def setup_board(self):
         self.board += [[Rook(-1), Knight(-1), Bishop(-1), Queen(-1), King(-1), Bishop(-1), Knight(-1), Rook(-1)]]
         self.board += [[Pawn(-1)] * 8]]
         self.board += [[[None] * 8]] * 4]
         self.board += [[Pawn(1)] * 8]]
         self.board += [[Rook(-1), Knight(-1), Bishop(-1), King(-1), Queen(-1), Bishop(-1), Knight(-1), Rook(-1)]]
+
+    def setup_pieces(self):
+        for x in range(8):
+            for y in range(8):
+                if self.board[y][x] != None:
+                    self.board[y][x].set_position(x, y)
     
+        
     def get_available_moves(self):
-        moves = []
-        for row in self.board:
-            for piece in row:
-                if piece != null and piece.color == self.current_color:
-                    for move in piece.get_moves(self):
-                        moves.append(((piece.x, piece.y), move))
+        for piece in self.pieces:
+            if piece.color == -1:
+                self.white_moves += coupler(piece.get_pos(), piece.get_available_moves())
+            if piece.color == 1:
+                self.black_moves += coupler(piece.get_pos(), piece.get_available_moves())
+                        
+    def empty(self, x, y):
+        return get(x, y) == None
     
     def get(self, x, y):
         return self.board[y][x]
     
     def get_king_pos(self):
-        for row in self.board:
-            for piece in row:
+        for piece in self.pieces:
                 if isinstance(piece, King) and piece.color == self.current_color:
                     return king.get_pos()
     
-    def leaves_check(self, move):
-        
-    
+    def in_check(self):
+        for piece in self.pieces:
+            if piece.color != self.current_color:
+                if self.
+            
     def move(self, move):
         self.board
